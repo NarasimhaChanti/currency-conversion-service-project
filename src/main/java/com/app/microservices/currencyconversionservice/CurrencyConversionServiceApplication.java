d8cdc171857763ab07941ec3a23bd8130e106eb8
NarasimhaChanti <springboot548@gmail.com>
2018-05-19 13:23:48 +0530

    currency-converter:ticket-2677

---
+++ src/main/java/com/app/microservices/currencyconversionservice/CurrencyConversionServiceApplication.java
@@ -0,0 +1,14 @@
+package com.app.microservices.currencyconversionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients("com.app.microservices.currencyconversionservice")
+public class CurrencyConversionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyConversionServiceApplication.class, args);
+	}
+}
---
+++ src/main/java/com/app/microservices/currencyconversionservice/proxy/CurrencyExchangeServiceProxy.java
@@ -0,0 +1,21 @@
+package com.app.microservices.currencyconversionservice.proxy;
+
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.app.microservices.currencyconversionservice.bean.CurrencyConversion;
+
+
+//@FeignClient(name="currency-exchange-service",url="http://localhost:8000")
+@FeignClient(name="currency-exchange-service")
+@RibbonClient(name="currency-exchange-service")
+public interface CurrencyExchangeServiceProxy {
+	
+	@GetMapping("/currency-exchange/from/{from}/to/{to}")
+	public CurrencyConversion retrieveExchangeValue(@PathVariable("from") String from,@PathVariable("to") String to);
+
+	
+		
+}
